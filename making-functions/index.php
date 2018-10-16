bc1e8ab116cef36f281a09992757618f1bc05778
Abel Trotter <a_trotter0@hotmail.com>
2018-10-16 12:04:50 -0700

    Add making-functions directory and solution

---
+++ making-functions/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Making Functions: Form</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+<div class="container">
+  <h1>Making Functions</h1>
+  <div>
+    <form action="results.php">
+      <h2>Fill out the fields to create an encrypted message!</h2>
+      <div class="form-group">
+        <label for="string1">String #1:</label>
+        <input type="text" name="string1" id="string1" class="form-control">
+      </div>
+      <div class="form-group">
+        <label for="string2">String #2:</label>
+        <input type="text" name="string2" class="form-control">
+      </div>
+      <div class="form-group">
+        <label for="string3">String #3:</label>
+        <input type="text" name="string3" class="form-control">
+      </div>
+      <button type="submit" class="btn btn-primary">Encrypt</button>
+    </form>
+  </div>
+</div>
+</body>
+</html>
---
+++ making-functions/results.php
@@ -0,0 +1,52 @@
+<?php
+  $text1 = $_GET["string1"];
+  $text2 = $_GET["string2"];
+  $text3 = $_GET["string3"];
+  $encrypted = encrypt($text1, $text2, $text3);
+
+  function encrypt($string1, $string2, $string3)
+  {
+    if ($string1 && $string2 && $string3) {
+      $reversed = strrev($string1);
+      $capitalized = strtoupper($string2);
+      $capitals_reversed = strtoupper(strrev($string3));
+      return $reversed . $capitalized . $capitals_reversed;
+    }
+  }
+
+  function display_results($encrypted_msg)
+  {
+    if ($encrypted_msg) {
+      echo "<h2>Here is your encrypted message!</h2>";
+      echo "<p>" . $encrypted_msg . "</p>";
+    } else {
+      echo "<h2>Please fill out all fields to display your message.</h2>";
+    }
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Making Functions: Results</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+<div class="container">
+  <h1>Making Functions</h1>
+  <div>
+    <h2>Original phrases:</h2>
+    <ul>
+      <li><?php echo $text1; ?></li>
+      <li><?php echo $text2; ?></li>
+      <li><?php echo $text3; ?></li>
+    </ul>
+    <?php display_results($encrypted); ?>
+  </div>
+</div>
+</body>
+</html>
