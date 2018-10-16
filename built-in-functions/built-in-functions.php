d013e5a010f46a2e77f59ee5f24efda379148a89
Abel Trotter <a_trotter0@hotmail.com>
2018-10-16 11:27:52 -0700

    Add built-in-functions repo and solution, add gitignore

---
+++ built-in-functions/built-in-functions.php
@@ -0,0 +1,31 @@
+<?php
+  $uppercase = strtoupper($_GET["string1"]);
+  $word_count = str_word_count($_GET["string2"]);
+  $shuffled = str_shuffle($_GET["string3"]);
+  $index_of_you = stripos($_GET["string4"], "you");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Built-in Functions: Result</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+  <div class="container">
+    <h1>Built-in Functions: Results</h1>
+    <h2>String 1: Uppercase </h2>
+    <p><?php echo $uppercase; ?></p>
+    <h2>String 2: Word Count</h2>
+    <p><?php echo $word_count; ?></p>
+    <h2>String 3: Shuffle</h2>
+    <p><?php echo $shuffled; ?></p>
+    <h2>String 4: First Occurrence of "you"</h2>
+    <p><?php echo $index_of_you; ?></p>
+  </div>
+</body>
+</html>
---
+++ built-in-functions/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Built-in Functions: Form</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+<div class="container">
+  <h1>Built-in Functions:</h1>
+  <div>
+    <form action="built-in-functions.php">
+      <div class="form-group">
+        <label for="string1">String #1:</label>
+        <input type="text" name="string1" id="string1" class="form-control">
+      </div>
+      <div class="form-group">
+        <label for="string2">String #2:</label>
+        <input type="text" name="string2" class="form-control">
+      </div>
+      <div class="form-group">
+        <label for="string3">String #3:</label>
+        <input type="text" name="string3" class="form-control">
+      </div>
+      <div class="form-group">
+        <label for="string4">String #4:</label>
+        <input type="text" name="string4" class="form-control">
+      </div>
+      <button type="submit" class="btn btn-primary">See What Happens</button>
+    </form>
+  </div>
+</div>
+</body>
+</html>
