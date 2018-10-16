2c157c76b5c17281dd3def3b87c3e1012eb51cb7
Abel Trotter <a_trotter0@hotmail.com>
2018-10-16 13:34:34 -0700

    Add branching directory, add solution

---
+++ branching/results.php
@@ -0,0 +1,33 @@
+<?php
+  $temp = $_GET["temp"];
+
+  function judgeTemp($temp)
+  {
+    if ($temp < 60) {
+      return "It's too cold out!";
+    } elseif ($temp > 80) {
+      return "It's too hot!";
+    } else {
+      return "It's lovely out!";
+    }
+  }
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Branching: Results</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+  <div class="container">
+    <h1>Branching: Current Temperature</h1>
+    <p><?php echo judgeTemp($temp); ?></p>
+  </div>
+</body>
+</html>
--- built-in-functions/index.php
+++ built-in-functions/index.php
@@ -9,29 +9,29 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
-<div class="container">
-  <h1>Built-in Functions:</h1>
-  <div>
-    <form action="built-in-functions.php">
-      <div class="form-group">
-        <label for="string1">String #1:</label>
-        <input type="text" name="string1" id="string1" class="form-control">
-      </div>
-      <div class="form-group">
-        <label for="string2">String #2:</label>
-        <input type="text" name="string2" class="form-control">
-      </div>
-      <div class="form-group">
-        <label for="string3">String #3:</label>
-        <input type="text" name="string3" class="form-control">
-      </div>
-      <div class="form-group">
-        <label for="string4">String #4:</label>
-        <input type="text" name="string4" class="form-control">
-      </div>
-      <button type="submit" class="btn btn-primary">See What Happens</button>
-    </form>
+  <div class="container">
+    <h1>Built-in Functions:</h1>
+    <div>
+      <form action="built-in-functions.php">
+        <div class="form-group">
+          <label for="string1">String #1:</label>
+          <input type="text" name="string1" id="string1" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="string2">String #2:</label>
+          <input type="text" name="string2" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="string3">String #3:</label>
+          <input type="text" name="string3" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="string4">String #4:</label>
+          <input type="text" name="string4" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-primary">See What Happens</button>
+      </form>
+    </div>
   </div>
-</div>
 </body>
 </html>
