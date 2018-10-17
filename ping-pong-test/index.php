d064dd54cd6968a75be084fb1a77a29aef022a36
Abel Trotter <a_trotter0@hotmail.com>
2018-10-17 15:03:50 -0700

    Fix indentation

--- ping-pong-test/index.php
+++ ping-pong-test/index.php
@@ -7,16 +7,16 @@
         integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <body>
-<div class="container">
-  <h1>Ping Pong Test</h1>
-  <p>Enter a number into the field below and witness the ping pong magic.</p>
-  <form action="results.php">
-    <label for="number">Number:</label>
-    <div class="form-group">
-      <input type="number" name="number" class="form-control">
-    </div>
-    <button type="submit" class="btn btn-primary btn-lg">Run Ping Pong</button>
-  </form>
-</div>
+  <div class="container">
+    <h1>Ping Pong Test</h1>
+    <p>Enter a number into the field below and witness the ping pong magic.</p>
+    <form action="results.php">
+      <label for="number">Number:</label>
+      <div class="form-group">
+        <input type="number" name="number" class="form-control">
+      </div>
+      <button type="submit" class="btn btn-primary btn-lg">Run Ping Pong</button>
+    </form>
+  </div>
 </body>
 </html>
